dfc9b11514c91d3c01a7ec1891dfee470422d5e2
dkt01 <d.k.turner@att.net>
2023-07-31 21:55:50 -0500

    Add arduino sketch to read line sensors

---
+++ arduino/line_sensor/line_sensor.ino
@@ -0,0 +1,20 @@
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  unsigned char buffer[8];
+  int leftSensorValue = analogRead(A0);
+  int centerSensorValue = analogRead(A1);
+  int rightSensorValue = analogRead(A2);
+  buffer[0] = (unsigned char)((leftSensorValue >> 0) & 0xff);
+  buffer[1] = (unsigned char)((leftSensorValue >> 8) & 0xff);
+  buffer[2] = (unsigned char)((centerSensorValue >> 0) & 0xff);
+  buffer[3] = (unsigned char)((centerSensorValue >> 8) & 0xff);
+  buffer[4] = (unsigned char)((rightSensorValue >> 0) & 0xff);
+  buffer[5] = (unsigned char)((rightSensorValue >> 8) & 0xff);
+  buffer[6] = '\r';
+  buffer[7] = '\n';
+  Serial.write(buffer, 8);
+  delay(20);
+}
